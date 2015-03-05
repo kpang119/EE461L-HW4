b672cbc58f418c19defc8f9fd7277664674e7354
Kelvin Pang <k.pang119@gmail.com>
2015-03-04 19:19:34 -0600

    Base Map
    Only has map

---
+++ EE461LHW4/app/src/androidTest/java/com/kkp452/ee461lhw4/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.kkp452.ee461lhw4;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
